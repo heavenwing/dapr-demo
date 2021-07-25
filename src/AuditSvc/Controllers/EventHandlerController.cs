afb5ea80417f547210e5e09090ec5746a33a7bee
Yongguang Zhu <Yongguang.Zhu@microsoft.com>
2021-07-25 18:29:08 +0800

    update statestore

--- src/AuditSvc/Controllers/EventHandlerController.cs
+++ src/AuditSvc/Controllers/EventHandlerController.cs
@@ -16,14 +16,27 @@ namespace AuditSvc.Controllers
     {
         [Topic("messages", "audit")]
         [HttpPost("auditplaced")]
-        public async Task PlaceAudit(AuditInfo info, [FromServices] DaprClient dapr, [FromServices] ILogger<EventHandlerController> logger)
+        public async Task<IActionResult> PlaceAudit(AuditInfo info, [FromServices] DaprClient dapr, [FromServices] ILogger<EventHandlerController> logger)
         {
-            logger.LogInformation("Got audit {Who} at {When} from {What}", info.Who,info.When,info.What);
+            logger.LogInformation("Got audit {Who} at {When} from {What}", info.Who, info.When, info.What);
 
-            if (string.IsNullOrEmpty(info?.Who)) return;
+            if (string.IsNullOrEmpty(info?.Who))
+            {
+                logger.LogWarning("Audit info's who is null or empty");
+                return Ok(new { status = "DROP" });
+            }
 
-            await dapr.SaveStateAsync("statestore", info.Who, info);
-            logger.LogInformation("Saved audit info into state management {Who}", info.Who);
+            try
+            {
+                await dapr.SaveStateAsync("statestore", info.Who, info);
+                logger.LogInformation("Saved audit info into state management {Who}", info.Who);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Failure saving audit info into state management {Who}", info.Who);
+                return NotFound();
+            }
         }
     }
 }
